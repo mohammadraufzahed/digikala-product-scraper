1eecce713b22190d85a89df2d3f3d5254963b2ec
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-05-03 16:51:40 +0430

    Add the init database

---
+++ init.py
@@ -0,0 +1,23 @@
+import os
+from colorama.ansi import Fore
+
+# Collect the data
+DB_HOST = str(input("Database Host: "))
+DB_USER = str(input("Database Username: "))
+DB_PASS = str(input("Database Password: "))
+DB_NAME = str(input("Database Name: "))
+
+# Create template
+data = f'''
+DB_HOST = str('{DB_HOST}')
+DB_USER = str('{DB_USER}')
+DB_PASS = str('{DB_PASS}')
+DB_NAME = str('{DB_NAME}')
+'''
+
+# Write the template to file
+with open(os.getcwd() + '/digikala_post/digikala_post/spiders/config/config.py', 'w+', encoding='utf8') as f:
+    f.flush()
+    f.write(data)
+    os.system(
+        f'autopep8 -i "{os.getcwd()}/digikala_post/digikala_post/spiders/config/config.py" ')
